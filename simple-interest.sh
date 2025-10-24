6225a64b46d82acb871ca872451ae2a6a631cb8e
tanvichillal <chillaltanvi@gmail.com>
2025-10-24 20:39:29 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Simple Interest Calculator
+# Formula: (P × R × T) / 100
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of Interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$((p * r * t / 100))
+echo "Simple Interest = $si"
 
